3a02ad985c04fb2782374f7a58c44b2b72a26c3d
art.gavrilov <art.gavrilov@vk.team>
2024-02-22 19:56:17 +0300

    correct some code lines

--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -17,7 +17,16 @@ export default async function AuthorLayout({
   content,
   params: { locale },
 }: AuthorLayoutProps) {
-  const { name, avatar, occupation, company, linkedin, instagram, email, telegram, youtube } = content
+  const { name,
+    avatar,
+    occupation,
+    company,
+    linkedin,
+    instagram,
+    email,
+    telegram,
+    youtube
+  } = content
   const { t } = await createTranslation(locale, 'about')
 
   return (
